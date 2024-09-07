bdd72a12d46513d4589811767549c9c0981f37f9
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-09-07 10:09:37 -0400

    Initial commit

---
+++ src/main/java/net/scarcekoi/moretags/MoreTagsClient.java
@@ -0,0 +1,10 @@
+package net.scarcekoi.moretags;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreTagsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/scarcekoi/moretags/MoreTagsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.scarcekoi.moretags;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreTagsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
